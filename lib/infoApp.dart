32cb57e1b899b77ba31ba1564bc314bb2739ee0a
msi <it@megroupco.net>
2023-01-07 13:47:12 +0200

    last V

---
+++ lib/infoApp.dart
@@ -0,0 +1,27 @@
+/*
+
+اسم المشروع : Chat Encryption
+
+
+     ****** عمل الطلاب ******
+ 1- عبدالله ماهر مصطفى الغفري 20192095
+ 2- أحمد سعيد أبو حمام 20192454
+ 3- أحمد محمد لبد 20191137
+ 4- محمد عوني ياسين 20192577
+
+
+ تحت اشراف الدكتور : أحمد محمود
+
+
+
+ Screens :
+ 1- splash Screen
+ 2- login screen
+ 3- SignUp screen
+ 4- forget password screen
+ 5- Users Screen
+ 6- chat screen
+
+
+
+ */
